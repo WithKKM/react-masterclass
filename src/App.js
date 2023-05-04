22ce8f7bf801a6c8438825839995c4c5de1781cf
WithKKM <201800535@hufs.ac.kr>
2023-05-05 03:56:54 +0900

    Part 2.6

--- src/App.js
+++ src/App.js
@@ -1,77 +1,27 @@
 import styled, {keyframes} from 'styled-components';
 
-const Wrapper = styled.div`
-  display: flex;
-`;
-// styled-component기본 사용법
-
-// const Box = styled.div`
-//   background-color: ${props => props.bgColor};
-//   width: 100px;
-//   height: 100px;
-// `;
-// // 컴포넌트 변경
-// const Circle = styled(Box)`
-//   border-radius: 50px;
-// `;
-// // 컴포넌트 확장
-// const Btn = styled.button`
-//   color: white;
-//   background-color: tomato;
-//   border: 0;
-//   border-radius: 15px;
-// `;
-// 버튼 만들기
-{
-  /* as="?"를 통해 태그를 바꿀 수 있음 */
-}
-// const Input = styled.input.attrs({required: true, minLength: 10})`
-//   background-color: tomato;
-// `;
-// 속성값 한 번에 넣는 법
-
-const rotationAnimation = keyframes`
-0% {
-  transform: rotate(0deg);
-  border-radius: 0px;
-}
-50% {
-  transform: rotate(360deg);
-  border-radius: 100px;
-}
-100%{
-  transform: rotate(0deg);
-  border-radius: 0px;
-}
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotationAnimation} 1s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 72px;
-    }
-    &:active {
-      opacity: 0;
-    }
+const Title = styled.h1`
+  color: tomato;
+  &:hover {
+    color: teal;
   }
 `;
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  ${Title}:hover {
+    font-size: 150px;
+  }
+`;
+
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😊</Emoji>
-      </Box>
-      <Emoji>🔥</Emoji>
+      <Title>Hello</Title>
+      <h1>Hello</h1>
     </Wrapper>
   );
 }
